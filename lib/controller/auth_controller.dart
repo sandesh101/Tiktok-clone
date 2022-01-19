f0a618f712474cd1636bce8a419f03adfa5fb898
Sandesh Rimal <rimal.sandesh11@gmail.com>
2022-01-19 21:43:34 +0545

    Added Navigator on Login

--- lib/controller/auth_controller.dart
+++ lib/controller/auth_controller.dart
@@ -4,13 +4,32 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:tiktok_clone/constant.dart';
+import 'package:tiktok_clone/views/screens/home_screen.dart';
+import 'package:tiktok_clone/views/screens/login_screen.dart';
 import '../models/users.dart' as model;
 
 class AuthController extends GetxController {
   //registering the User into the APP
   static AuthController instance = Get.find();
+  late Rx<User?> _user;
   late Rx<File?> _pickedImage;
   File? get profilePhoto => _pickedImage.value;
+
+  @override
+  void onReady() {
+    super.onReady();
+    _user = Rx<User?>(firebaseAuth.currentUser);
+    _user.bindStream(firebaseAuth.authStateChanges());
+    ever(_user, _setInitialScreen);
+  }
+
+  _setInitialScreen(User? user) {
+    if (user == null) {
+      Get.offAll(() => LoginScreen());
+    } else {
+      Get.offAll(() => const HomeScreen());
+    }
+  }
 
   void pickImage() async {
     final pickedImage =
