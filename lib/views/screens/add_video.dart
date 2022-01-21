682e139632539807eab532b55b21723dd1bce232
Sandesh Rimal <rimal.sandesh11@gmail.com>
2022-01-21 22:15:22 +0545

    Added dialog options

--- lib/views/screens/add_video.dart
+++ lib/views/screens/add_video.dart
@@ -5,12 +5,67 @@ import '../../constant.dart';
 class AddVideo extends StatelessWidget {
   const AddVideo({Key? key}) : super(key: key);
 
+  showOptionDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (context) => SimpleDialog(
+        children: [
+          SimpleDialogOption(
+            onPressed: () {},
+            child: Row(
+              children: const [
+                Icon(Icons.image),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    "Gallery",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SimpleDialogOption(
+            onPressed: () {},
+            child: Row(
+              children: const [
+                Icon(Icons.camera),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    "Camera",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SimpleDialogOption(
+            onPressed: () {},
+            child: Row(
+              children: const [
+                Icon(Icons.cancel),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    "Cancel",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: InkWell(
-          onTap: () {},
+          onTap: () => showOptionDialog(context),
           child: Container(
             height: 50,
             width: 190,
